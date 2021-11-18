76b230285c9a732200842844e07cdb3a7361d876
Krzysztof 'Dalton' Pachulski <krzysztof.pachulski@gmail.com>
2021-11-18 14:13:03 +0100

    PoC Pack all static mesh actors into a PackedLevelInstance

--- Content/Python/quick_tools.py
+++ Content/Python/quick_tools.py
@@ -1,22 +1,34 @@
 import unreal
 
-def populate_the_level(num_actor = 10000, mesh = '/Engine/BasicShapes/Cube.Cube', radius = 128000):
+def populate(num_actor = 1000, mesh = '/Engine/BasicShapes/Cube.Cube', radius = 12800):
     desired_world_box_extent = unreal.Vector(radius, radius, radius)
     
     for i in range(0, num_actor):
-        spawn_location = unreal.Vector().random_point_in_box_extents(desired_world_box_extent)
+        # print(desired_world_box_extent.__dict__)
+        spawn_location = unreal.Vector().random_point_in_box_extents(half_size=desired_world_box_extent)
         spawn_rotation = unreal.MathLibrary.random_rotator(True)
         spawned_actor = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.StaticMeshActor.static_class(), spawn_location, spawn_rotation)
         static_mesh = unreal.EditorAssetLibrary.load_asset(mesh)
+        print(dir(spawned_actor))
         spawned_actor.static_mesh_component.set_editor_property('static_mesh', static_mesh)
+
+# https://docs.unrealengine.com/4.27/en-US/ProductionPipelines/ScriptingAndAutomation/Python/
+# total_frames = 100
+# text_label = "Working!"
+# with unreal.ScopedSlowTask(total_frames, text_label) as slow_task:
+#     slow_task.make_dialog(True)               # Makes the dialog visible, if it isn't already
+#     for i in range(total_frames):
+#         if slow_task.should_cancel():         # True if the user has pressed Cancel in the UI
+#             break
+#         slow_task.enter_progress_frame(1)     # Advance progress by one frame.
+#                                             # You can also update the dialog text in this call, if you want.
+#         ...                                   # Now do work for the current frame here!
 
 def pack_all(cell_size = 6400.0):
     world = unreal.UnrealEditorSubsystem().get_editor_world()
     actors = unreal.GameplayStatics.get_all_actors_of_class(world, unreal.StaticMeshActor.static_class())
     
     level_bounds = unreal.GameplayStatics.get_actor_array_bounds(actors, False)
-    bounds_min = level_bounds.origin - level_bounds.box_extent
-    bounds_max = level_bounds.origin + level_bounds.box_extent
 
     cells = {}
     cells['empty_cell'] = []
@@ -26,7 +38,7 @@ def pack_all(cell_size = 6400.0):
         x = int(actor_location.x / cell_size)
         y = int(actor_location.y / cell_size)
         z = int(actor_location.z / cell_size)
-        cell_name = x + '_' + y + '_' + z
+        cell_name = str(x) + '_' + str(y) + '_' + str(z)
 
         if cell_name in cells:
             cells[cell_name].append(actor)
